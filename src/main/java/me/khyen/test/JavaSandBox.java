e1d99590b4dfbb4aee71469bc539d0e1d7ae9b89
Kevin Yen <ky.17364@gmail.com>
2016-01-07 21:29:01 -0800

    Initial commit

---
+++ src/main/java/me/khyen/test/JavaSandBox.java
@@ -0,0 +1,16 @@
+package me.khyen.test;
+
+import me.khyen.test.exception.JavaSandBoxException;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * @author Kevin Yen
+ */
+
+public class JavaSandBox {
+
+	public static void main(String [] args) throws Exception {
+
+	}
+}
---
+++ src/main/java/me/khyen/test/exception/JavaSandBoxException.java
@@ -0,0 +1,23 @@
+package me.khyen.test.exception;
+
+/**
+ * @author Kevin Yen
+ */
+public class JavaSandBoxException extends Exception {
+
+	public JavaSandBoxException() {
+	}
+
+	public JavaSandBoxException(String msg) {
+		super(msg);
+	}
+
+	public JavaSandBoxException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public JavaSandBoxException(Throwable cause) {
+		super(cause);
+	}
+
+}
