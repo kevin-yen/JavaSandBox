c95c1ab06278460138cce95431024b96cc536267
Kevin Yen <kevin.yen@liferay.com>
2017-08-29 16:53:19 -0700

    Run through AddWebContentPage examples

--- src/main/java/me/khyen/test/JavaSandBox.java
+++ src/main/java/me/khyen/test/JavaSandBox.java
@@ -11,6 +11,23 @@ public class JavaSandBox {
 	public static void main(String [] args) throws Exception {
 		WebDriver webDriver = WebDriverFactory.getWebDriver();
 
+		HomePage homePage = new HomePage(webDriver);
+
+		homePage.open();
+
+		homePage.signIn("test@liferay.com", "test");
+
+		WebContentPage webContentPage = homePage.openWebContentPage();
+
+		AddWebContentPage addWebContentPage = webContentPage.openAddWebContentPage();
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<h2 class=\"text-center\">Hello</h2>\n");
+		sb.append("<p class=\"text-center\">World!</p>");
+
+		addWebContentPage.addSourceWebContent(sb.toString());
+
 		webDriver.quit();
 	}
 
