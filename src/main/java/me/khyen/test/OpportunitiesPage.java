06b73edc8df49421cb99226f5b87e46a22c6f106
Kevin Yen <kevin.yen@liferay.com>
2017-09-01 17:27:33 -0700

    Open find page

--- src/main/java/me/khyen/test/OpportunitiesPage.java
+++ src/main/java/me/khyen/test/OpportunitiesPage.java
@@ -1,12 +1,15 @@
 package me.khyen.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class OpportunitiesPage extends Page {
 
 	private static By populateOtherProjectLink = By.linkText("Populate Other-Populate Other Project-S-2013.1.1");
-	private static By partnerResellerField = By.id("CF00N70000002Obry_ileinner");
+	private static By partnerResellerField = By.id("CF00N70000002Obry_ilecell");
 	private static By searchIcon = By.id("CF00N70000002ObryIcon");
 	private static By detailsLink = By.id("efpViews_0062F000002O3RB_option1");
 
@@ -21,14 +24,7 @@ public class OpportunitiesPage extends Page {
 
 		click(partnerResellerField);
 
-		try {
-			Thread.sleep(100);
-		}
-		catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		click(partnerResellerField);
+		type(partnerResellerField, Keys.ENTER);
 
 		click(searchIcon);
 	}
