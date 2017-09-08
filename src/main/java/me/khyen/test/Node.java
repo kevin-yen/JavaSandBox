ada09505f3c2c4bbc33e684a71c382a7e1e7c931
Kevin Yen <kevin.yen@liferay.com>
2017-09-08 12:07:23 -0700

    Update node fields with more relevent types

---
+++ src/main/java/me/khyen/test/Node.java
@@ -0,0 +1,107 @@
+package me.khyen.test;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Node {
+
+	private Node parent;
+	private String name;
+	private Date timestamp;
+	private long duration;
+	private List<Node> children = new ArrayList<>();
+
+	public Node(Node parent, String name, Date timestamp) {
+		this.parent = parent;
+		this.name = name;
+		this.timestamp = timestamp;
+	}
+
+	public void add(Node node) {
+		children.add(node);
+	}
+
+	public Node add(Node parent, String name, Date timestamp) {
+		Node childNode = new Node(parent, name, timestamp);
+
+		children.add(childNode);
+
+		return childNode;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{\"name\":\"");
+		sb.append(name);
+		sb.append("\",\"timestamp\":");
+		sb.append(timestamp.getTime());
+		sb.append(",\"duration\":");
+		sb.append(duration);
+
+		if (children.size() > 0) {
+			sb.append(",\"children\":[");
+
+			boolean first = true;
+
+			for (Node node : children) {
+				if (first) {
+					first = false;
+				}
+				else {
+					sb.append(",");
+				}
+
+				sb.append(node.toString());
+			}
+
+			sb.append("]");
+		}
+
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	public JSONObject toJSONObject() {
+		JSONArray jsonArray = new JSONArray();
+
+		for (Node node : children) {
+			jsonArray.put(node.toJSONObject());
+		}
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("name", name);
+		jsonObject.put("timestamp", timestamp.getTime());
+		jsonObject.put("duration", duration);
+		jsonObject.put("children", jsonArray);
+
+		return jsonObject;
+	}
+
+}
